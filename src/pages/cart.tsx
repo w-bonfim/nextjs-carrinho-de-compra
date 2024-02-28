c04e6242e9d0b9b30b1c7850d07e8f2bf8fa6a8a
Wellington Bonfim <wellington.bonfim@hotmail.com>
2024-02-28 11:30:35 -0300

    first commit

---
+++ src/pages/cart.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Cart: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Carrinho</title>
+                <meta name="description" content="Conheça todos os nossos produtos" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>
+                Carrinho
+            </h1>
+        </>
+    )
+}
+
+export default Cart
