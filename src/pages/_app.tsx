2225c9757694c67f1ccd18634182f6ad9e3b64fc
Wellington Bonfim <wellington.bonfim@hotmail.com>
2024-03-06 16:25:14 -0300

    Aplicação finalizada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { CartContextTypeProvider } from '@/hooks/useCart'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CartContextTypeProvider>
+      <Component {...pageProps} />
+    </CartContextTypeProvider>
+  )
 }
